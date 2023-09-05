f00842e23d0ae13fd46891d1bb3bfdc45d5997df
Benjamin Enadeba <qg002757@student.reading.ac.uk>
2023-09-05 23:12:38 +0100

    Extras

--- Main.c
+++
@@ -1,7 +0,0 @@
-//This will be our main entry point
-#include "WindowSetup.h"
-
-int main(void) {
-	WindowSetup();
-	return 0;
-}
--- WindowSetup.h
+++
@@ -1,9 +0,0 @@
-//Here we will create our window
-#include <stdio.h>
-#include <SDL.h>
-#include <stdbool.h>
-void WindowSetup(void);
-void UserInput(void);
-void CleanUp(void);
-void init_map(void);
-void load_blocks(void);
