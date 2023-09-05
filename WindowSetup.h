7ba78a6110918a349a60c9b873bed430580f3013
Benjamin Enadeba <qg002757@student.reading.ac.uk>
2023-09-05 22:59:05 +0100

    Header file

---
+++ WindowSetup.h
@@ -0,0 +1,9 @@
+//Here we will create our window
+#include <stdio.h>
+#include <SDL.h>
+#include <stdbool.h>
+void WindowSetup(void);
+void UserInput(void);
+void CleanUp(void);
+void init_map(void);
+void load_blocks(void);
